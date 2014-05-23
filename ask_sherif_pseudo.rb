1db3a53739b414152889d66f4aa47d713a921711
bootcoder <bootcoder@gmail.com>
2014-05-23 12:48:00 -0700

    update pseudo

---
+++ ask_sherif_pseudo.rb
@@ -0,0 +1,53 @@
+
+def ask_question
+
+end
+
+def answer_question
+
+end
+
+def add_answer
+
+end
+
+def delete_answer
+
+end
+
+def list_answers
+
+end
+
+def exit (double_enter)
+
+end
+
+
+
+
+
+=begin
+
+display ascii art of sherifs face.
+
+store data via txt file -- open at run save at exit
+
+questions via gets.chomp
+
+CLEAR screen from start
+	
+use random number to determine weather to use 'general canned' or 'interpolated canned response'
+
+data structure will be a hash where 
+	:key is the keyword
+	:value is array of possible answers
+
+select values randomly.. (sample)
+
+
+
+
+
+	
+=end
