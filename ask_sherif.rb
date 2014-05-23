b3c1faea48ddf85b213b8a13d3e009fc8c67e2e5
bootcoder <bootcoder@gmail.com>
2014-05-23 16:31:44 -0700

    answers done

--- ask_sherif.rb
+++ ask_sherif.rb
@@ -1,29 +1,58 @@
 
-def ask_question
-
-end
-
-def answer_question
-
-end
-
-def add_answer
-
-end
-
-def delete_answer
-
-end
-
-def list_answers
-
-end
-
-def exit (double_enter)
-
-end
 
 
+answers = {hash: [hash_answers].sample, git: [git_answers].sample, string: [string_answers].sample, array: [array_answers].sample}
+
+
+hash_answers = 	['Hashes are kind of like your walking in the forest, it doesn\'t matter how you get our of the forest,
+ 									right, just GTFO','Hashes,,,yeah so you just kinda have to get in there and figure it out right?',
+ 									'So yeah, hashes are a mother fucker at the beginning, just keep swimming man, you\'ll get there.',
+ 									'IDK man, IDK...',
+ 									'']
+
+git_answers = 	['Git is a lot like Kevin Bacon.........',
+								'Yeah, Github...Just do the Branches challenge and you\'ll be totally fine.',
+								'So Keven Bacon is all like swoosh wizz bang, and the General is all like "AHHHHHHHHHHHH"',
+								" http://www.ruby-doc.org/core-1.9.3/Hash.html \n http://www.tutorialspoint.com/ruby/ruby_hashes.htm \n http://ruby.about.com/od/rubyfeatures/a/hashes.htm \n http://stackoverflow.com/questions/8415240/merge-ruby-hash",
+								"Are you locked in? No,, OK, well we\' talk about that later.",
+								"You're trading your time for something here, right, does that make sense. So like, figure out what you want and trade your time for that right..."]
+
+general_answers = ["So there\'s this farmer right.",
+									"This whole thing is you\'re experience, ya get me, it\'s like a choose your own adventure book",
+									"What the shit?",
+									"So there\'s these blind men and an elephant,,,,",
+									"I mean, it\'s not fucking difficult right.",
+									"It's just syntax, it doesn\'t matter.",
+									"Does it really matter",
+									"So you\'re trading your time for some magic sauce right, but the truth is there is no fucking magic sauce. You just have to be the shit for you...",
+									"There was this traveler and a wise man...Long story short, they drink a lot and get high.",
+									"There is no magic sauce for your bag of tricks, like that\'s not why you\'re here."]
+
+
+
+string_answers = ['Strings are just another data structure end of discussion end of story',
+ 					        'Strings are just texts',
+        					"If you don't know Strings by now...ask more questions!!",
+        					"Strings are just...Strings!",
+        					"You\'re trading your time here right, for strings, I mean c\'mon."]
+
+array_answers = ['Arrays are just another data structure end of discussion end of story',
+        					"If you care about order use Arrays..everybody cool?",
+        					"If you don't know Arrays by now...ask more questions!!",
+        					"What the shit?! Arrays just store things...cool?"]
+
+finisher = ["Ya know?", 
+						'You know what I\'m saying?', 
+						"Everybody Cool?", 
+						'Are you following me?',
+						"Am I making sense?", 
+						'ya get what I\'m saying?', 
+						"OK so if you're solid you can bail, I don't care.",
+						"Like if you\'re good then great, but if not let me know. Like now is the time. Don\'t wait.",
+						"Yeah man, that\'s great, but whatever, ya know?",
+						"Whatever, it doesn\'t matter. Am I making sense?",
+						"Maybe,,, You tell me!",
+						"Is that OK?"]
 
 
 
